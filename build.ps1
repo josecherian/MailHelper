6f96d50793787841eea54caf386908e79e6c0f6a
Jose <josecherian09@gmail.com>
2019-04-17 04:30:03 +0800

    Bumping version to !deploy

--- build.ps1
+++ build.ps1
@@ -3,7 +3,8 @@ param ($Task = 'Default')
 # Grab nuget bits, install modules, set build variables, start build.
 Get-PackageProvider -Name NuGet -ForceBootstrap | Out-Null
 
-Install-Module Psake, PSDeploy, BuildHelpers, CredencialManager -force
+Install-Module Psake, PSDeploy, BuildHelpers -Force
+Install-Module CredentialManager -RequiredVersion 2.0 -Force
 Install-Module Pester -Force -SkipPublisherCheck
 Import-Module Psake, BuildHelpers
 
